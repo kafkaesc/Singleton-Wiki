95a8c02f250017f829e59cf8b54a49a55512511c
kafkaesc <jared.a.hettinger@gmail.com>
2023-12-28 13:05:44 -0600

    Adding wiki edit and display components

---
+++ src/components/SoloDisplay.tsx
@@ -0,0 +1,5 @@
+import P from '@/elements/P';
+
+export default function SoloDisplay() {
+	return <P>The wiki data</P>;
+}
---
+++ src/components/SoloEditor.tsx
@@ -0,0 +1,4 @@
+export default function SoloEditor() {
+	// TODO: style this
+	return <textarea></textarea>;
+}
