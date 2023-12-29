ef413fd33b386152f8a70ed50f4828c384644835
kafkaesc <jared.a.hettinger@gmail.com>
2023-12-29 13:55:31 -0600

    Creating the PageColumn component and applying it to page components

--- src/app/Test/page.tsx
+++ src/app/Test/page.tsx
@@ -1,11 +1,12 @@
 import H1 from '@/elements/H1';
 import P from '@/elements/P';
+import PageColumn from '@/components/PageColumn';
 
 export default function Test() {
 	return (
-		<>
+		<PageColumn>
 			<H1>Test Page</H1>
 			<P>the test page</P>
-		</>
+		</PageColumn>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,34 +3,38 @@ import H1 from '@/elements/H1';
 import H2 from '@/elements/H2';
 import Link from '@/elements/Link';
 import P from '@/elements/P';
+import PageColumn from '@/components/PageColumn';
 
 export default function Home() {
 	return (
 		<main>
-			<H1 className="text-2xl">Home Page</H1>
-			<P>This is the home page</P>
-			<Link className="mx-1" href="/About">
-				About
-			</Link>
-			<Link className="mx-1" href="/Test">
-				Test
-			</Link>
-			<H2>Button tests</H2>
-			<Button>Default</Button>
-			<Button buttonStyle="primary">Primary</Button>
-			<Button buttonStyle="secondary">Secondary</Button>
-			<Button buttonStyle="text">Text</Button>
-			<H2>Disabled Buttons</H2>
-			<Button disabled>Default</Button>
-			<Button buttonStyle="primary" disabled={true}>
-				Primary
-			</Button>
-			<Button buttonStyle="secondary" disabled={true}>
-				Secondary
-			</Button>
-			<Button buttonStyle="text" disabled={true}>
-				Text
-			</Button>
+			<PageColumn>
+				<h1 className="text-8xl">{`{1}`}</h1>
+				<H1 className="text-2xl">The Solo Wiki</H1>
+				<P>This is the home page</P>
+				<Link className="mx-1" href="/About">
+					About
+				</Link>
+				<Link className="mx-1" href="/Test">
+					Test
+				</Link>
+				<H2>Button tests</H2>
+				<Button>Default</Button>
+				<Button buttonStyle="primary">Primary</Button>
+				<Button buttonStyle="secondary">Secondary</Button>
+				<Button buttonStyle="text">Text</Button>
+				<H2>Disabled Buttons</H2>
+				<Button disabled>Default</Button>
+				<Button buttonStyle="primary" disabled={true}>
+					Primary
+				</Button>
+				<Button buttonStyle="secondary" disabled={true}>
+					Secondary
+				</Button>
+				<Button buttonStyle="text" disabled={true}>
+					Text
+				</Button>
+			</PageColumn>
 		</main>
 	);
 }
