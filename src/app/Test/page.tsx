4672687d60ac7a364a439e38465dd42881c4f547
kafkaesc <jared.a.hettinger@gmail.com>
2023-12-30 09:25:36 -0600

    Adding items to the test page

--- src/app/Test/page.tsx
+++ src/app/Test/page.tsx
@@ -5,6 +5,8 @@ import H1 from '@/elements/H1';
 import H2 from '@/elements/H2';
 import P from '@/elements/P';
 import PageColumn from '@/components/PageColumn';
+import TopNav from '@/components/TopNav';
+import WelcomeBox from '@/components/WelcomeBox';
 
 export const metadata: Metadata = {
 	title: 'Test | Solo Wiki | Built by Jared Hettinger',
@@ -13,25 +15,29 @@ export const metadata: Metadata = {
 
 export default function Test() {
 	return (
-		<PageColumn>
-			<H1>Test Page</H1>
-			<P>the test page</P>
-			<H2>Button tests</H2>
-			<Button>Default</Button>
-			<Button buttonStyle="primary">Primary</Button>
-			<Button buttonStyle="secondary">Secondary</Button>
-			<Button buttonStyle="text">Text</Button>
-			<H2>Disabled Buttons</H2>
-			<Button disabled>Default</Button>
-			<Button buttonStyle="primary" disabled={true}>
-				Primary
-			</Button>
-			<Button buttonStyle="secondary" disabled={true}>
-				Secondary
-			</Button>
-			<Button buttonStyle="text" disabled={true}>
-				Text
-			</Button>
-		</PageColumn>
+		<>
+			<TopNav />
+			<PageColumn>
+				<WelcomeBox />
+				<H1>Test Page</H1>
+				<P>the test page</P>
+				<H2>Button tests</H2>
+				<Button>Default</Button>
+				<Button buttonStyle="primary">Primary</Button>
+				<Button buttonStyle="secondary">Secondary</Button>
+				<Button buttonStyle="text">Text</Button>
+				<H2>Disabled Buttons</H2>
+				<Button disabled>Default</Button>
+				<Button buttonStyle="primary" disabled={true}>
+					Primary
+				</Button>
+				<Button buttonStyle="secondary" disabled={true}>
+					Secondary
+				</Button>
+				<Button buttonStyle="text" disabled={true}>
+					Text
+				</Button>
+			</PageColumn>
+		</>
 	);
 }
