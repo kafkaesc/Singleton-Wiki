96cd4057cfbbea7e70b0d3690c3c9b3536003ac9
kafkaesc <jared.a.hettinger@gmail.com>
2023-12-30 09:25:16 -0600

    Adding the welcome box and top nav to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,27 @@
-import H1 from '@/elements/H1';
 import Link from '@/elements/Link';
 import P from '@/elements/P';
 import PageColumn from '@/components/PageColumn';
+import TopNav from '@/components/TopNav';
+import WelcomeBox from '@/components/WelcomeBox';
 
 export default function Home() {
 	return (
-		<main>
-			<PageColumn>
-				<h1 className="text-8xl">{`{1}`}</h1>
-				<H1 className="text-2xl">The Solo Wiki</H1>
-				<P>This is the home page</P>
-				<Link className="mx-1" href="/About">
-					About
-				</Link>
-				<Link className="mx-1" href="/Test">
-					Test
-				</Link>
-			</PageColumn>
-		</main>
+		<>
+			<TopNav />
+			<main>
+				<PageColumn>
+					<WelcomeBox />
+					<div className="text-center">
+						<P>This is the home page</P>
+						<Link className="mx-1" href="/About">
+							About
+						</Link>
+						<Link className="mx-1" href="/Test">
+							Test
+						</Link>
+					</div>
+				</PageColumn>
+			</main>
+		</>
 	);
 }
