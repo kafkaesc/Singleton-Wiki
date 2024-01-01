3fe01a64f31cb19e166a96254059f3f40eac8a51
kafkaesc <jared.a.hettinger@gmail.com>
2023-12-31 18:31:43 -0600

    Updating TopNav to receive and pass css class names

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,9 +1,31 @@
 import Link from '@/elements/Link';
 import PageColumn from '@/components/PageColumn';
 
-export default function TopNav() {
-	return (
-		<PageColumn>
+interface TopNavProps {
+	className?: string;
+}
+
+export default function TopNav({ className }: TopNavProps) {
+	return className ? (
+		<PageColumn
+			className={`border border-0 border-b border-wi-gray+2 ${className}`}
+		>
+			<div className="flex items-center min-h-14">
+				<div className="flex-none">
+					<h1 className="text-4xl">Solo Wiki</h1>
+				</div>
+				<div className="text-right grow">
+					<Link className="mx-2" href="/">
+						Create Account
+					</Link>
+					<Link className="mx-2" href="/">
+						Login
+					</Link>
+				</div>
+			</div>
+		</PageColumn>
+	) : (
+		<PageColumn className="border border-0 border-b-1 border-wi-gray+2">
 			<div className="flex items-center min-h-14">
 				<div className="flex-none">
 					<h1 className="text-4xl">Solo Wiki</h1>
