d7fc07f6ba1fd5359e971b2baaa1d992aab649b9
kafkaesc <jared.a.hettinger@gmail.com>
2024-01-02 17:17:51 -0600

    Updating the login page and login form

--- src/app/LogIn/page.tsx
+++ src/app/LogIn/page.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 
 import H1 from '@/elements/H1';
-import OneToFourColumn from '@/components/OneToFourColumn';
 import LogInForm from '@/components/LogInForm';
+import OneAndFourColumn from '@/components/OneAndFourColumn';
 import PageColumn from '@/components/PageColumn';
 import TopNav from '@/components/TopNav';
 
@@ -18,9 +18,9 @@ export default function LogIn() {
 			<main>
 				<PageColumn>
 					<H1>Log in to Solo Wiki</H1>
-					<OneToFourColumn>
+					<OneAndFourColumn>
 						<LogInForm />
-					</OneToFourColumn>
+					</OneAndFourColumn>
 				</PageColumn>
 			</main>
 		</>
