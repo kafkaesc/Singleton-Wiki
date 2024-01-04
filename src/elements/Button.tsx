cc04cb106e746fbbd1c59f5a42294b8390ae6db2
kafkaesc <jared.a.hettinger@gmail.com>
2024-01-04 12:41:16 -0600

    Ordering element attributes in Button component

--- src/elements/Button.tsx
+++ src/elements/Button.tsx
@@ -16,16 +16,16 @@ function PrimaryButton({
 	return className ? (
 		<button
 			{...props}
-			disabled={!!disabled}
 			className={`px-2 text-wi-black bg-wi-blue enabled:hover:bg-wi-blue-dark enabled:hover:underline disabled:cursor-not-allowed ${className}`}
+			disabled={!!disabled}
 		>
 			{children}
 		</button>
 	) : (
 		<button
 			{...props}
-			disabled={!!disabled}
 			className="px-2 text-wi-black bg-wi-blue enabled:hover:bg-wi-blue-dark enabled:hover:underline disabled:cursor-not-allowed"
+			disabled={!!disabled}
 		>
 			{children}
 		</button>
@@ -41,16 +41,16 @@ function SecondaryButton({
 	return className ? (
 		<button
 			{...props}
-			disabled={!!disabled}
 			className={`px-2 text-wi-black bg-wi-green enabled:hover:bg-wi-green-dark enabled:hover:underline disabled:cursor-not-allowed ${className}`}
+			disabled={!!disabled}
 		>
 			{children}
 		</button>
 	) : (
 		<button
 			{...props}
-			disabled={!!disabled}
 			className="px-2 text-wi-black bg-wi-green enabled:hover:bg-wi-green-dark enabled:hover:underline disabled:cursor-not-allowed"
+			disabled={!!disabled}
 		>
 			{children}
 		</button>
@@ -66,16 +66,16 @@ function TextButton({
 	return className ? (
 		<button
 			{...props}
-			disabled={!!disabled}
 			className={`px-2 enabled:text-link-blue disabled:text-wi-black enabled:hover:bg-wi-gray enabled:hover:underline disabled:cursor-not-allowed ${className}`}
+			disabled={!!disabled}
 		>
 			{children}
 		</button>
 	) : (
 		<button
 			{...props}
-			disabled={!!disabled}
 			className="px-2 enabled:text-link-blue disabled:text-wi-black enabled:hover:bg-wi-gray enabled:hover:underline disabled:cursor-not-allowed"
+			disabled={!!disabled}
 		>
 			{children}
 		</button>
