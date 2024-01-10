39b4fd55b929bdb2810b4b1dbecf63f9e5986231
kafkaesc <jared.a.hettinger@gmail.com>
2024-01-10 10:18:32 -0600

    Updating text size on H1 and H2 element components

--- src/elements/H1.tsx
+++ src/elements/H1.tsx
@@ -8,11 +8,11 @@ interface H1Props {
 
 export default function H1({ children, className, ...props }: H1Props) {
 	return className ? (
-		<h1 {...props} className={`text-4xl text-wi-black ${className}`}>
+		<h1 {...props} className={`text-5xl text-wi-black ${className}`}>
 			{children}
 		</h1>
 	) : (
-		<h1 {...props} className="text-4xl text-wi-black">
+		<h1 {...props} className="text-5xl text-wi-black">
 			{children}
 		</h1>
 	);
